40d522740ae521f43920a72d5dfb7c987f184823
Dante <114578362+GetLovv@users.noreply.github.com>
2023-10-06 13:39:14 +0200

    Update app.js

--- lekcja4/app.js
+++ lekcja4/app.js
@@ -1,11 +1,9 @@
+//zadanie 1
 let x = document.getElementById("myForm");
 x.addEventListener("focusin", myFocusFunction);
-x.addEventListener("focusout", myBlurFunction);
 
 function myFocusFunction() {
   document.getElementById("onfocus").style.backgroundColor = "yellow";
 }
 
-function myBlurFunction() {
-  document.getElementById("onfocus").style.backgroundColor = "";
-}
+//zadanie 2
