9137591e1e4580bd274aec39aa5571d4939ad540
Dante <mciolak@o2.pl>
2023-11-13 11:01:16 +0100

    4324
    234

---
+++ lekcja13/zad2/app.js
@@ -0,0 +1,17 @@
+
+function allowDrop(jablka) {
+    jablka.preventDefault();
+}
+
+function drag(jablka) {
+    jablka.dataTransfer.setData("text", jablka.target.id);
+}
+
+function drop(jablka) {
+    jablka.preventDefault();
+    var data = jablka.dataTransfer.getData("text");
+    if (jablka.target.tagName == 'TD') {
+        jablka.target.appendChild(document.getElementById(data));
+}
+
+}
