2e9e1f4300af1c2282b9f8311a6a4f4d6c3f53b1
Dante <114578362+GetLovv@users.noreply.github.com>
2023-11-19 14:26:47 +0100

    23
    23

---
+++ Prace Domowe/Pętle/elementy_input.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var form = document.getElementById('myForm');
+
+  for (var i = 1; i <= 100; i++) {
+    var input = document.createElement('input');
+    input.type = 'text';
+
+    var id = (i < 10) ? 'input_0' + i : 'input_' + i;
+    input.id = id;
+    input.name = 'input_' + i;
+    input.placeholder = 'Element numer ' + i;
+    input.style.margin = '10px';
+    input.style.boxShadow = '5px 5px 5px lightblue';
+    form.appendChild(input);
+
+    if (i % 5 === 0) {
+      form.appendChild(document.createElement('br'));
+    }
+  }
+});
